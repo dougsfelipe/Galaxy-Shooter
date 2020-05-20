e7c3dae1213ac71905a82f46d35a22aaa8729ff7
dougsfelipe <dfcs@cin.ufpe.br>
2020-05-20 01:41:42 -0300

    Create scripts for moving

---
+++ Assets/Galaxy Shooter/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //Variables
+
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * speed * VerticalInput);
+      
+      
+    }
+}
