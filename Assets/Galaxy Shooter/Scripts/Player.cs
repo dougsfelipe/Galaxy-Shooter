84ef6c221a8d07102b751aeefa60da4055cbbbc9
dougsfelipe <dfcs@cin.ufpe.br>
2020-05-21 23:32:53 -0300

    Criando nave e laser

--- Assets/Galaxy Shooter/Scripts/Player.cs
+++ Assets/Galaxy Shooter/Scripts/Player.cs
@@ -6,23 +6,56 @@ public class Player : MonoBehaviour
 {
 
     //Variables
-
-    public float speed = 5.0f;
+    public GameObject lasePrefabs;
+    [SerializeField] private float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0,0,0);
+        transform.position = new Vector3(0, 0, 0);
+        Debug.Log("Game begins");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //restrições da tela
+        Movement();
+
+        if( Input.GetKeyDown(KeyCode.Space)){
+            //spawn laser
+            Instantiate(lasePrefabs, transform.position + new Vector3( 0 , 0.88f , 0 ), Quaternion.identity);
+        }
+    }
+
+    private void Movement()
+    {
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float VerticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
         transform.Translate(Vector3.up * Time.deltaTime * speed * VerticalInput);
-      
-      
+
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.x > 9.4f)
+        {
+            transform.position = new Vector3(-9.4f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.4f)
+        {
+            transform.position = new Vector3(9.4f, transform.position.y, 0);
+        }
+
     }
+
+
 }
